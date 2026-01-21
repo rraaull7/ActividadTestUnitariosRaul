f98e75100bf436fb934f2afb865d08c504195376
rraaull7 <rraaulldemiguel@gmail.com>
2026-01-21 12:31:25 +0100

    hola

---
+++ src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
@@ -0,0 +1,29 @@
+package es.fplumara.dam1.operaciones;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OperacionesTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,INSUFICIENTE",
+            "7.3,INSUFICIENTE"
+    })
+    @DisplayName("Comprueba las notas")
+    public void clasificacionNotasValidas(double nota, String resultadoEsperado) {
+        String resultado = Operaciones.calificacion(nota);
+        assertEquals(resultadoEsperado,resultado);
+    }
+
+
+
+
+
+
+
+
+}
